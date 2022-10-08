54f752ebd64ba4fdea0e9283e339d5a6d4a936e8
Abbas Al Khalifa <info@devf5r.com>
2022-10-08 04:00:25 +0300

    vvvv

--- index.js
+++ index.js
@@ -12,25 +12,26 @@ const twilioObj = {
     cred : null 
 }
 
-// Voice chat uses turn server, not required locally 
-if(process.env.USE_TWILIO==="yes") { 
-    const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+// Comment if not required => below code is used to change creadentials for TURN server 
+//======================================================================================
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+client.tokens.create().then(token => {
+    twilioObj.username = token.username;
+    twilioObj.cred = token.password; 
+});
+
+//every 12 hours 
+schedule.scheduleJob("*/12 * * *",()=>{
+    console.log("CRON running"); 
+    const client = require('twilio')(process.env.accountSid, process.env.authToken);
     client.tokens.create().then(token => {
         twilioObj.username = token.username;
         twilioObj.cred = token.password; 
     });
+    controlRooms.deQRoom();
+})
+//========================================================================
 
-    //every 12 hours 
-    schedule.scheduleJob("*/12 * * *",()=>{
-        console.log("CRON running"); 
-        const client = require('twilio')(process.env.accountSid, process.env.authToken);
-        client.tokens.create().then(token => {
-            twilioObj.username = token.username;
-            twilioObj.cred = token.password; 
-        });
-        controlRooms.deQRoom();
-    })
-}
 
 const cors = require('cors');
 const app = express(); 
