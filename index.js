d7a766ab5d725ec60e710fcf18b7c598ede3e6d9
Abbas Al Khalifa <info@devf5r.com>
2022-10-10 03:27:43 +0300

    zzzzzzz

--- index.js
+++ index.js
@@ -1,97 +1,90 @@
-require('events').EventEmitter.prototype._maxListeners = 0;
+const express = require("express");
+const app = express();
+const room = require("./routes/room");
 
-const express = require('express')
-var bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express()
-const { RoomHandler, User } = require('./room')
-var ExpressPeerServer = require('peer').ExpressPeerServer;
+const cors = require("cors");
+require("dotenv").config();
+const { PORT, DATABASE_URI, CLIENT_URL, NODE_ENV } = process.env;
 
-const server = require('https').createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/socket.capstone-cryptalk.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/socket.capstone-cryptalk.com/fullchain.pem'),
-  requestCert: false,
-  rejectUnauthorized: false
-}, app)
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const corsConfig = {
+    origin: CLIENT_URL, //react frontend url
+    methods: ["GET", "POST", "DELETE", "PUT"]
+}
+const io = new Server(server, {
+    cors: corsConfig
+});
 
-const roomHandler = new RoomHandler()
+const mongoose = require("mongoose");
+mongoose.connect(DATABASE_URI);
 
-app.use(express.static(__dirname + '/'));
-app.use(bodyParser.urlencoded({extend:true}));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.use(express.json());
+app.use(cors());
 
-app.get('/', function (req, res) {
-  res.render("index", {
-    rooms: roomHandler.rooms
-  })
-})
-
-const io = require('socket.io')(server, {
-  cors: {
-    origins: ["*"]
-  }
-})
-
-app.use('/peer', ExpressPeerServer(server, {
-    debug: true
-  }
-));
-
-io.on('connection', (socket) => {
-  console.log(`. > ${socket.id}`)
-
-
-  socket.on('room::join', (roomID, user) => {
-    roomHandler.initializeRoom(roomID)
-    const room = roomHandler.getRoomById(roomID)
-
-    room.join(new User(socket.id, user.username, user.sub))
-
-    socket.join(roomID)
-    socket.to(roomID).emit('room::userJoined', user)
-
-    // Data storage for later disconnect cleanup
-    socket.userData = user
-    socket.currentRoom = roomID
-  })
-
-  socket.on('room::leave', (roomID, user) => {
-    const room = roomHandler.getRoomById(roomID)
-    socket.leave(roomID)
-    room.leave(user.sub)
-    socket.to(roomID).emit('room::userLeft', user)
-    socket.currentRoom = null
-  })
-
-  // UI Listeners
-  socket.on('room::enableVoice', (roomID, user) => {
-    socket.to(roomID).emit('room::enableVoice', user)
-  })
-
-  socket.on('room::disableVoice', (roomID, user) => {
-    socket.to(roomID).emit('room::disableVoice', user)
-  })
-
-  socket.on('room::enableVideo', (roomID, user) => {
-    socket.to(roomID).emit('room::enableVideo', user)
-  })
-
-  socket.on('room::disableVideo', (roomID, user) => {
-    socket.to(roomID).emit('room::disableVideo', user)
-  })
-
-  // On socket disconnection
-  socket.on('disconnect', () => {
-    console.log(`- > ${socket.id}`)
-    roomHandler.removeBySocket(socket.id)
-    if (socket.currentRoom !== null) {
-      socket.to(socket.currentRoom).emit('room::userLeft', socket.userData)
+app.use(cors({
+    origin: (origin, callback) => {
+        if (origin !== CLIENT_URL && NODE_ENV === "production") {
+            return callback("Access to this host is denied", false);
+        } else {
+            return callback(null, true);
+        }
     }
-  })
+}));
+app.use("/room", room);
+const Room = require("./model/roomModel");
+
+io.on("connection", (socket) => {
+    socket.on("join", (data, callback) => {
+        socket.join(data);
+        console.log(`${socket.id} joined room ${data}`);
+        socket.to(data).emit("newJoin", socket.id);
+        callback(socket.id);
+    })
+
+    socket.on("sendMsg", (data) => {
+        socket.broadcast.to(data.room).emit("receiveMsg", data.msg);
+    })
+
+    socket.on("disconnect", () => {
+        async function handleDisc() {
+            let room = await Room.findOne({ "users.userID": socket.id });
+            if (room) {
+                let updatedRm = await Room.findOneAndUpdate({ _id: room._id }, { $pull: { users: { userID: socket.id }, voiceUsers: { userID: socket.id } } }, { new: true });
+                if (updatedRm.users.length === 0) {
+                    await Room.findOneAndDelete({ _id: room._id });
+                }
+            }
+        }
+        handleDisc();
+        socket.broadcast.emit("disc", socket.id);
+    });
+
+    /* webRTC connections */
+    socket.on("joinVoice", (roomID) => {
+        async function handleVoiceConnect(roomID) {
+            let room = await Room.findOne({ _id: roomID });
+            let users = room.voiceUsers.filter((user) => {
+                return user.userID !== socket.id;
+            })
+            socket.emit("allUsers", users);
+        }
+        handleVoiceConnect(roomID);
+    });
+
+    socket.on("sendSgn", (payload) => {
+        io.to(payload.userToSignal).emit("voiceJoined", { signal: payload.signal, caller: payload.caller });
+    });
+
+    socket.on("returnSgn", (payload) => {
+        io.to(payload.caller).emit("receiveSgn", { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on("voiceConfig", (payload) => {
+        socket.broadcast.emit("voiceConfig", payload);
+    })
+
 })
 
-server.listen(443)
-console.clear()
-console.log('[server] $ Server started \n')
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
