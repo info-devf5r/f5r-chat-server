304baea8181a0d3b3f6f567431e8044abf496409
Abbas Al Khalifa <info@devf5r.com>
2022-10-10 05:33:04 +0300

    zxv

--- Makefile
+++
@@ -1,8 +0,0 @@
-setup:
-	docker volume create chat_backend_node_modules
-
-install:
-	docker compose -f compose.builder.yaml run --rm install
-
-dev:
-	docker compose -f compose.yaml up -d
--- validation/auth.js
+++
@@ -1,8 +0,0 @@
-const yup = require('yup');
-
-exports.loginSchema = yup.object().shape({
-    body: yup.object({
-        username: yup.string().required(),
-        password: yup.string().required(),
-    })
-});
